b3bf95c97f590be166f8b133d7ab3b75f2fd3572
蛯名　麗斗 <uu071978@CA1816.local>
2017-09-21 17:48:22 +0900

    wei

--- lib/Common.py
+++ lib/Common.py
@@ -9,22 +9,24 @@ class Common():
     GOOGLE_SEACH_API_KEY = os.environ["GOOGLE_SEACH_API_KEY"]
     CUSTOM_SEARCH_ENGINE = os.environ["CUSTOM_SEARCH_ENGINE"]
 
-    img_list = []
+    img_dict = {}
     i = 0
     while i < total_num:
       query_img = "https://www.googleapis.com/customsearch/v1?key=" + GOOGLE_SEACH_API_KEY + "&cx=" + CUSTOM_SEARCH_ENGINE + "&num=" + str(10 if(total_num-i)>10 else (total_num-i)) + "&start=" + str(i+1) + "&q=" + quote(search_item) + "&searchType=image"
-      print (query_img)
+      #print (query_img)
       res = urllib.request.urlopen(query_img)
       data = json.loads(res.read().decode('utf-8'))
       for j in range(len(data["items"])):
-        img_list.append(data["items"][j]["image"]["thumbnailLink"])
-        #img_list.append(data["items"][j]["link"])
+        img_dict["thumbnail"] = data["items"][j]["image"]["thumbnailLink"]
+        img_dict["link"] = data["items"][j]["link"]
+        #img_dict.append(data["items"][j]["image"]["thumbnailLink"])
+        #img_dict.append(data["items"][j]["link"])
       i=i+10
-    return img_list
+    return img_dict
 
 
 if __name__ == "__main__":
   args = sys.argv
   common = Common()
-  img_list = common.getImageUrl(args[1], 1)
-  print(img_list)
+  img_dict = common.getImageUrl(args[1], 1)
+  print(img_dict)
